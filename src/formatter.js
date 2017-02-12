f7b54013cef95d05124f8e9463a45f116c4e2612
viktorstrate <viktorstrate@gmail.com>
2017-02-12 20:14:13 +0100

    Small improvements

--- src/formatter.js
+++ src/formatter.js
@@ -41,6 +41,15 @@ const formatter = (parsedLatex) => {
         }
       }
 
+      if (item.value === 'sqrt') {
+        if (parsedLatex[i + 1].type === 'group') {
+          console.log('Found square root')
+          formattedString += 'sqrt(' + parsedLatex[i + 1].value + ')'
+        }
+      } else {
+        console.log('Square root did not have any following parameters, ignoring')
+      }
+
       if (item.value === 'cdot' || item.value === 'times' || item.value === 'ast') {
         formattedString += '*'
       }
