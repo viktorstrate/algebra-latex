cae76c44dc6276a1557cf16c4b55bfb6bc0ce286
viktorstrate <viktorstrate@gmail.com>
2017-03-19 20:58:39 +0100

    Made the package useable through NPM, and updated to 0.2.0

--- src/index.js
+++ src/index.js
@@ -2,13 +2,28 @@ import algebra from 'algebra.js'
 import parseLatex from './parser'
 import formatLatex from './formatter'
 
-const structure = parseLatex('2^{10}')
-console.log(JSON.stringify(structure, null, 2))
-
+// Functors
 const stripParenthesis = mathString => mathString.substr(1, mathString.length - 2)
-const formattedMath = stripParenthesis(formatLatex(structure))
-console.log('Formatted math:', formattedMath)
 
-// eslint-disable-next-line new-cap
-const exp = new algebra.parse(formattedMath)
-console.log('Parsed math:', exp)
+export default class AlgebraLatex {
+  constructor (latex) {
+    this.texInput = latex
+
+    this.structure = parseLatex(latex)
+  }
+
+  toMath () {
+    if (this.formattedMath == null) {
+      this.formattedMath = stripParenthesis(formatLatex(this.structure))
+    }
+
+    return this.formattedMath
+  }
+
+  toAlgebra () {
+    if (this.parsedAlgebra == null) {
+      this.parsedAlgebra = algebra.parse(this.toMath())
+    }
+    return this.parsedAlgebra.toString()
+  }
+}
